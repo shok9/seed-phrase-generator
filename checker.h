d8b151de5363a987fd3cc6a0d24b9c6f37eaa005
Cosmo11 <qwertokondo@gmail.com>
2024-06-11 21:14:19 +0300

    Update checker.h

--- checker.h
+++ checker.h
@@ -10,11 +10,11 @@ struct balance
 
 std::vector<std::string> websites = { "https://blockscan.com/", "https://etherscan.io/", "https://www.blockchain.com/explorer", "https://privatekeyfinder.io/mnemonic-converter", "https://mempool.space/", "https://live.blockcypher.com/ltc/", "https://dogechain.info/"};
 
-std::string get_wallet_adress_from_mnemonic(std::string mnemonic);
-std::string get_private_key_form_mnemonic(std::string mnemonic);
-
 std::string read_url(std::string url);
 DWORD post_request(std::string url, std::string request, DWORD* code, std::string* response);
+
+std::string get_wallet_adress_from_mnemonic(std::string mnemonic);
+std::string get_private_key_form_mnemonic(std::string mnemonic);
 
 DWORD check_wallet(std::string mnemonic, balance* wallet_balance);
 
